5f804e13e552afd40f0aebd9342ff7b420f614d0
farishadi18-stack <farishadi18@gmail.com>
2025-08-27 01:56:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title(' Prediction Factor of Safety')
 
-st.info('This is app create prediction using soil nailing!')
+st.info('This is app create prediction using soil nailing!!')
