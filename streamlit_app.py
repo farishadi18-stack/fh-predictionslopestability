3813ea6757339506eb18a4a39eb1909ede8fb45e
farishadi18-stack <farishadi18@gmail.com>
2025-08-27 08:30:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,3 @@ st.info('This is app create prediction using soil nailing !!')
 with st.expander('Data'):
   st.write('Raw Data')
   df = pd.read_csv('https://raw.githubusercontent.com/farishadi18-stack/fh-predictionslopestability/refs/heads/master/new%20treated%20slope.csv')
-  
