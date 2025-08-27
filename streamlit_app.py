4dc4fb6f79114157ee90f060eba957513c86751d
farishadi18-stack <farishadi18@gmail.com>
2025-08-27 08:29:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.title(' Prediction Factor of Safety')
 
@@ -8,4 +10,4 @@ st.info('This is app create prediction using soil nailing !!')
 with st.expander('Data'):
   st.write('Raw Data')
   df = pd.read_csv('https://raw.githubusercontent.com/farishadi18-stack/fh-predictionslopestability/refs/heads/master/new%20treated%20slope.csv')
-  df
+  
