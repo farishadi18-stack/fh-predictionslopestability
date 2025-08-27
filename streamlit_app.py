a964cfb9eaba72d0c51423861d00a8330e4c40c5
farishadi18-stack <farishadi18@gmail.com>
2025-08-27 08:25:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.title(' Prediction Factor of Safety')
 
-st.info('This is app create prediction using soil nailin !!')
+st.info('This is app create prediction using soil nailing !!')
+
+# Step 1: Load Data
+with st.expander('Data'):
+  st.write('Raw Data')
+  df = pd.read_csv('https://raw.githubusercontent.com/ArifAzhar243/artificialneuralnetworkbyaa/refs/heads/master/aa%20Machine%20Learning.csv')
+  df
